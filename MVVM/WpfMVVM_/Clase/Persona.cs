f41b30fd3556ac73a0cd380453d3c09ecf31179f
joahnfire11 <71619492+joahnfire11@users.noreply.github.com>
2023-12-17 18:51:30 -0500

    Add files via upload

---
+++ MVVM/WpfMVVM_/Clase/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpMVVM.Clase
+{
+    class Persona
+    {
+        public string _nombres;
+        public string Nombres
+        {
+            get { return this._nombres; }
+            set { this._nombres = value; }
+        }
+        public string _apellidos;
+        public string Apellidos
+        {
+            get { return this._apellidos; }
+            set { this._apellidos = value; }
+        }
+        public string _telefono;
+        public string Telefono
+        {
+            get { return this._telefono; }
+            set { this._telefono = value; }
+        }
+        public string _Correo;
+        public string Correo
+        {
+            get { return this._Correo; }
+            set { this._Correo = value; }
+        }
+
+    }
+}
---
+++ MVVM/WpfMVVM_/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpMVVM.Clase;
+
+namespace WpMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            Persona persona = new Persona
+            {
+                Nombres = "Johan Xavier",
+                Apellidos = "Palma Palma",
+                Telefono = "0989033014",
+                Correo = "e1313788356@live.uleam.edu.ec"
+            };
+            this.DataContext = persona;
+        }
+    }
+}
